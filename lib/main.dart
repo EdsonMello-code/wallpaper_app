b576ee6bf437dfc69a508d1d4da3260f0dbe8566
EdsonMello-code <edsonmelo754@gmail.com>
2023-07-23 20:06:43 -0300

    feat:  change hive inicialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() {
-  Hive.init('/storage/emulated/0/Download/');
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final applicationDirectory = await getApplicationDocumentsDirectory();
+
+  Hive.init(applicationDirectory.path);
   runApp(
     ModularApp(
       module: AppModule(),
